0c812fe435d650f2b25a99191f69e16ee38a6078
hefvcjm <1020657447@qq.com>
2019-01-22 12:47:32 +0800

    新建device文件夹，添加注释

---
+++ data/device/box/Box.py
@@ -0,0 +1,41 @@
+# coding = utf-8
+from data.model import *
+
+
+class Box(Model.Model):
+    class BoxConfig(Config.Config):
+
+        def __init__(self):
+            super().__init__()
+            self.V_max = 0  # 最大输入电压
+            self.V_min = 0  # 最小输入电压
+            self.I_max = 0  # 最大输入电流
+            self.N = 0  # 并联组串数
+            self.I_out_max = 0  # 最大输出电流
+
+    class BoxInput(Input.Input):
+
+        def __init__(self):
+            super().__init__()
+            self.serials = []  # 组串
+
+    class BoxOutput(Output.Output):
+
+        def __init__(self):
+            super().__init__()
+            self.I = 0  # 输出电流
+            self.V = 0  # 输出电压
+
+    def __init__(self, m_input=None, m_output=None, m_config=None):
+        super().__init__(m_input, m_output, m_config)
+        if m_config is None:
+            self.config = self.BoxConfig()
+
+        if m_input is None:
+            self.input = self.BoxInput()
+
+        if m_output is None:
+            self.output = self.BoxOutput()
+
+        self.loss = 0
+        self.T = 0
---
+++ data/device/serial/Serial.py
@@ -0,0 +1,38 @@
+# coding = utf-8
+from data.model import *
+
+
+class Serial(Model.Model):
+    class SerialConfig(Config.Config):
+
+        def __init__(self):
+            super().__init__()
+            self.N = 0  # 组串个数
+
+    class SerialInput(Input.Input):
+
+        def __init__(self):
+            super().__init__()
+            self.modules = []  # 组件
+
+    class SerialOutput(Output.Output):
+
+        def __init__(self):
+            super().__init__()
+            self.I = 0  # 输出电流
+            self.V = 0  # 输出电压
+
+    def __init__(self, m_input=None, m_output=None, m_config=None):
+        super().__init__(m_input, m_output, m_config)
+
+        if m_input is None:
+            self.input = self.SerialInput()
+
+        if m_output is None:
+            self.output = self.SerialOutput()
+
+        if m_config is None:
+            self.config = self.SerialConfig()
+
+        self.loss = 0
+        self.T = 0
